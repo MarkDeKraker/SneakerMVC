3f7bc7d0792e0cbb0ec541ddc7480becbdb50876
Mark de Kraker <mark.de.kraker@outlook.com>
2023-10-11 22:09:17 +0200

    Added new dashboard page and applied styling to navbar

---
+++ SneakerMVC/resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('content')
+
+<div class="container text-green-600 text-4xl text-center"></div>
+
+@endsection
--- SneakerMVC/resources/views/layouts/app.blade.php
+++ SneakerMVC/resources/views/layouts/app.blade.php
@@ -16,7 +16,6 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
 
             <!-- Page Heading -->
             @if (isset($header))
@@ -26,11 +25,27 @@
                     </div>
                 </header>
             @endif
+        <div class="bg-gray-white shadow-lg p-5">
+            <div class="flex justify-between container mx-auto">
+            <p class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Sneaker MVC</p>
+            @if (Route::has('login'))   
+                <div class="">
+                    @auth
+                        <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                    @else
+                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+            </div>
+            
+        </div>
+            @yield('content')
+
         </div>
     </body>
 </html>
