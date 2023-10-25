79cf904c8199f565d633beba98835286d483bac7
Mark de Kraker <mark.de.kraker@outlook.com>
2023-10-25 15:19:21 +0200

    Homepage styling

--- SneakerMVC/resources/views/home.blade.php
+++ SneakerMVC/resources/views/home.blade.php
@@ -1,15 +1,46 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="mt-20">
-    <div class="">
-    <p class="text-center font-medium text-4xl">Welcome to SneakerMVC</p>
-    <p class="text-center font-medium text-xl">The place to keep track of your sneakers</p>
-    <div class="flex justify-center mt-2 space-x-2">
-        <a href="{{ route('register') }}" class="bg-black text-white p-2 rounded-lg">Register</a>
-        <a href="{{ route('login') }}" class="bg-black text-white p-2 rounded-lg">Login</a>
+<div class="container mx-auto shadow-lg mt-10 p-2">
+    <p class="font-medium text-xl">Marketplace</p>
+    <div class="bg-white grid grid-cols-4 gap-2">
+    <div class="border-2 max-w-xs rounded-lg p-2">
+        <div class="grid grid-cols-2">
+            <div>
+                <p>Brand: Nike</p>
+                <p>Model: Air Jordan 1</p>
+                <p>Color: Black/white</p>
+            </div>
+        </div>
+        <div class="my-2">
+            <a class="bg-black text-white p-2 rounded-lg">Details</a>
+        </div>
     </div>
+    <div class="border-2 max-w-xs rounded-lg p-2">
+        <div class="grid grid-cols-2">
+            <div>
+                <p>Brand: Nike</p>
+                <p>Model: Air Jordan 1</p>
+                <p>Color: Black/white</p>
+            </div>
+        </div>
+        <div class="my-2">
+            <a class="bg-black text-white p-2 rounded-lg">Details</a>
+        </div>
     </div>
+    <div class="border-2 max-w-xs rounded-lg p-2">
+        <div class="grid grid-cols-2">
+            <div>
+                <p>Brand: Nike</p>
+                <p>Model: Air Jordan 1</p>
+                <p>Color: Black/white</p>
+            </div>
+        </div>
+        <div class="my-2">
+            <a class="bg-black text-white p-2 rounded-lg">Details</a>
+        </div>
+    </div>
+
 </div>
 
 @endsection
